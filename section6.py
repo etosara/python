69b8bbd5b92a76e5f46d67968439b94c5b67854b
etosara <10820326+etosara@users.noreply.github.com>
2018-07-26 13:40:08 -0400

    First substance
    I added a comment and a new function for the second commit of this file.

--- section6.py
+++ section6.py
@@ -1,1 +1,13 @@
 # This is a first Python file.  I'd like to create a .ipynb file, but this will do.
+
+# Now I'll add some code
+
+import numpy as np
+import pandas as pd
+
+# the succeeding function generatens numerical months
+def thing(integer):
+    m = integer
+    while True:
+        m =m%12 + 1
+        yield m
